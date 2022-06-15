5a3ef0de01b39bec3a82ee338cf973026e118ee8
danielsblj <dani_herrera_macias@hotmail.com>
2022-06-15 15:04:29 -0500

    Poner model varios y crear super usuario de Maria

--- PortalEmpleado/App_PortalEmpleado/polls/admin.py
+++ PortalEmpleado/App_PortalEmpleado/polls/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import TipoDocumento
+from .models import TipoDocumento, Ciudad, Personas, TipoContrato
 
 admin.site.register(TipoDocumento)
+admin.site.register(Ciudad)
+admin.site.register(Personas)
+admin.site.register(TipoContrato)
 
--- PortalEmpleado/App_PortalEmpleado/polls/models.py
+++ PortalEmpleado/App_PortalEmpleado/polls/models.py
@@ -21,3 +21,31 @@ class TipoDocumento(models.Model):
 
     def __str__(self):
         return self.nombre_documento
+
+class Ciudad(models.Model):
+    nombre_ciudad = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.nombre_ciudad
+
+class TipoContrato(models.Model):
+    nombre_tipo_contrato = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.nombre_tipo_contrato
+
+class Personas(models.Model):
+    numero_documento = models.CharField(max_length=20)
+    primer_nombre = models.CharField(max_length=70)
+    otros_nombres = models.CharField(max_length=70, null=True, blank=True)
+    primer_apellido = models.CharField(max_length=70)
+    otros_apellidos = models.CharField(max_length=70, null=True, blank=True)
+    correo_electronico = models.CharField(max_length=70)
+    telefono = models.CharField(max_length=50)
+    perfil = models.TextField(max_length=250)
+    foto_persona = models.ImageField(null=True, blank=True)
+    tipo_documento = models.ForeignKey(TipoDocumento, on_delete=models.CASCADE)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.numero_documento
